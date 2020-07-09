c375d3ca3e55c4dfce952d8cc7dbd58c6f6fec7f
Alexei Kekin <tcore.ak@gmail.com>
2020-07-09 22:12:05 +0300

    Updates

---
+++ precache-manifest.d86cc7ad73f389174cbd876f5c6a6430.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6160387bbc52054691c1a2f6173c323c",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "4fbf4f6a59d79f0799dd",
+    "url": "/react-app/static/css/main.3635ea9e.chunk.css"
+  },
+  {
+    "revision": "39fde8e885fcb5e3f8e3",
+    "url": "/react-app/static/js/2.43dd3253.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.43dd3253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fbf4f6a59d79f0799dd",
+    "url": "/react-app/static/js/main.21143ab9.chunk.js"
+  },
+  {
+    "revision": "e9c3f32e1ebc5a0ec784",
+    "url": "/react-app/static/js/runtime-main.b0dbe400.js"
+  },
+  {
+    "revision": "0124d24c831bacdc14030e1eabfbebcd",
+    "url": "/react-app/static/media/wallpaper.0124d24c.jpg"
+  }
+]);
